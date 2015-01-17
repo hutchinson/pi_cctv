9197d47fb624de1e6d9d93bd0a8c6c322f2487f7
Dan Hutchinson <d.hutchinson4@googlemail.com>
2015-01-17 16:44:51 +0000

    Gets the basic StorageHandler registration working

--- lib/capture_manager.rb
+++ lib/capture_manager.rb
@@ -3,6 +3,8 @@
 # post capture handlers and storage handler.
 #
 
+require 'singleton'
+
 module PiCctv
   # Capture handlers can be executed either before the active storage handler
   # is run (in which case they can modify the image being stored), or post
@@ -29,12 +31,53 @@ module PiCctv
     def store(image_path, context)
 
     end
+
+    # Should return name of the active storage handler.
+    def name()
+
+    end
   end
 
   # The CaptureManager is responsible for scheduling images to be taken on the
   # given schedule and calling the registered CaptureHandlers and
   # StorageHandlers.
   class CaptureManager
+    include Singleton
 
+    def initialize()
+      @active_storage_handler = nil
+    end
+
+    # Set the active storage handler
+    #
+    # TODO: Look into passing the Class rather than a instance, then we
+    # can create instances of the storage handler on demand.
+    def set_active_storage_handler(new_handler)
+      if new_handler == nil or not new_handler.kind_of? StorageHandler
+        puts "Warning: Storage handler must not be nil and must derive from StorageHandler."
+        return
+      end
+
+      if @active_storage_handler != nil
+        puts "Replacing '#{@active_storage_handler.name}' with '#{new_handler.name}'"
+      end
+
+      @active_storage_handler = new_handler
+    end
   end
 end
+
+class TestStorageHandler < PiCctv::StorageHandler
+
+  def store(image_path, context)
+    puts(image_path, context)
+  end
+
+  # Should return name of the active storage handler.
+  def name()
+    "TestStorageHandler"
+  end
+end
+
+PiCctv::CaptureManager.instance.set_active_storage_handler TestStorageHandler.new
+PiCctv::CaptureManager.instance.set_active_storage_handler TestStorageHandler.new
